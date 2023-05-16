40bd23b7524c0b87cb3aa420105acece75a780d1
Ilia <118176700+Mazhorov21Ilia@users.noreply.github.com>
2023-05-16 10:42:57 +0300

    Update and rename Module3_numbers.py(in progress) to Module3_Numbers.py

--- Neural_networks/Module3_Numbers.py
+++ Neural_networks/Module3_Numbers.py
@@ -1,24 +1,38 @@
 import torch
 import random
 import numpy as np
+
+
 random.seed(0)
 np.random.seed(0)
 torch.manual_seed(0)
 torch.cuda.manual_seed(0)
 torch.backends.cudnn.deterministic = True
+
+
 import torchvision.datasets
 MNIST_train = torchvision.datasets.MNIST('./', download=True, train=True)
 MNIST_test = torchvision.datasets.MNIST('./', download=True, train=False)
+
+
 X_train = MNIST_train.train_data
 y_train = MNIST_train.train_labels
 X_test = MNIST_test.test_data
 y_test = MNIST_test.test_labels
+
+
 X_train = X_train.float()
 X_test = X_test.float()
+
+
 X_train.shape, X_test.shape
 y_train.shape, y_test.shape
+
+
 X_train = X_train.reshape([-1, 28 * 28])
 X_test = X_test.reshape([-1, 28 * 28])
+
+
 class MNet(torch.nn.Module):
     def __init__(self, n_hidden_neurons):
         super(MNet, self).__init__()
@@ -33,9 +47,15 @@ class MNet(torch.nn.Module):
         return x
     
 mnist_net = MNet(100)
+
+
 loss = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(mnist_net.parameters(), lr=0.001)
+
+
 batch_size = 70
+
+
 for epoch in range(10000):
     order = np.random.permutation(len(X_train))
     
